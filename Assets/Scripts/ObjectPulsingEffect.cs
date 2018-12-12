a9d9b2fe7a4700506cb675dbd6d9df21093346fd
LukasMachala <mac0348@vsb.cz>
2018-12-12 21:27:40 +0100

    Úpravy pulsing effectu pro navigaci

--- Assets/Scripts/ObjectPulsingEffect.cs
+++ Assets/Scripts/ObjectPulsingEffect.cs
@@ -3,48 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectPulsingEffect : MonoBehaviour {
+    public enum ScaleDirection { FORWARD, BACKWARD }
+
+    [Header("Pulse object")]
     public GameObject objectToScale;
 
-    public float fromScale = 5.0f;
-    public float toScale = 0.0f;
+    [Header("Scale range")]
+    public float fromScale = 0.0f;
+    public float toScale = 1.0f;
 
+    public ScaleDirection scaleDirection;
+
+    [Header("Other properties")]
     public float speedOfTransition = 0.1f;
     
     public int countOfWaves = 5;
 
-    private Vector3 defaultScale;
+    private Vector3 _defaultScale;
+
+    private float _scaleBetweenWaves;
 
     private List<float> _actualScales = new List<float>();
     private List<GameObject> _scaleObjects = new List<GameObject>();
 
     private void Start()
     {
-        float scaleBetween = fromScale / countOfWaves;
+        _defaultScale = objectToScale.transform.localScale;
+
+        _scaleBetweenWaves = toScale / countOfWaves;
 
         for (int i = 0; i < countOfWaves; i++)
         {
-            _scaleObjects.Add(Instantiate(objectToScale, transform));
+            var scale = fromScale + (i * _scaleBetweenWaves);
 
-            _actualScales.Add(fromScale - (i * scaleBetween));
-        }
+            var generatedObject = Instantiate(objectToScale, transform);
+            generatedObject.transform.localScale *= scale;
 
-        defaultScale = objectToScale.transform.localScale;
-    }
-
-    private void FixedUpdate()
-    {
-        for (int i = 0; i < countOfWaves; i++)
-        {
-            _actualScales[i] -= Time.deltaTime * speedOfTransition;
-
-            if (_actualScales[i] < toScale)
-            {
-                _actualScales[i] = fromScale;
-            }
-
-            Vector3 objectScale = defaultScale * _actualScales[i];
-
-            _scaleObjects[i].transform.localScale = objectScale;
+            _scaleObjects.Add(generatedObject);
+            _actualScales.Add(scale);
         }
     }
+
+    //private void Update()
+    //{
+    //    var direction = scaleDirection == ScaleDirection.FORWARD ? 1 : -1;
+
+    //    for (int i = 0; i < countOfWaves; i++)
+    //    {
+    //        _actualScales[i] += direction * (Time.deltaTime * speedOfTransition);
+
+    //        if (_actualScales[i] < fromScale)
+    //        {
+    //            _actualScales[i] = toScale;
+    //        }
+
+    //        Vector3 objectScale = _defaultScale * _actualScales[i];
+
+    //        _scaleObjects[i].transform.localScale = objectScale;
+    //    }
+    //}
 }
